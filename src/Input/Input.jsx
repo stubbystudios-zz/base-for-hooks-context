005d24dad6dfce2ccf486ea2c6d5f428a0374121
Carey Best <careybest@Careys-MBP.attlocal.net>
2020-10-25 09:44:02 -0500

    Tests for currentGuess

--- src/Input/Input.jsx
+++ src/Input/Input.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Input(props) {
+function Input({ secretWord }) {
+  const [currentGuess, setCurrentGuess] = React.useState('');
+
   return (
-    <section data-test='input-component'>
-      <input type='text' />
-    </section>
-  )
+    <div className='container'>
+      <section data-test='input-component'>
+        <form>
+          <input
+            data-test='input-box'
+            className='input-box'
+            type='text'
+            placeholder='enter guess'
+            value={currentGuess}
+            onChange={(e) => setCurrentGuess(e.target.value)} />
+          <button
+            data-test='submit-button'
+            className='submit-button'>
+            Submit
+          </button>
+        </form>
+      </section>
+    </div>
+  );
+}
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 }
 
 export default Input;
--- src/Input/Input.test.jsx
+++ src/Input/Input.test.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { findByTestAttr } from '../../test/testUtils';
+import { findByTestAttr, checkProps } from '../../test/testUtils';
 import Input from './Input';
+import { valueOf } from 'prop-types/lib/ReactPropTypesSecret';
 
-const setup = () => {
-  return shallow(<Input />)
+const setup = (secretWord = 'party') => {
+  return shallow(<Input secretWord={secretWord} />)
 }
 
 test('renders without error', () => {
   const wrapper = setup();
   const inputComponent = findByTestAttr(wrapper, 'input-component');
   expect(inputComponent.length).toBe(1);
-}); 
+});
+
+test('does not throw warning with expected props', () => {
+  checkProps(Input, { secretWord: 'party' })
+});
+
+describe('state controlled input field', () => {
+  test('state updates with value of input box upon change', () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+
+    const mockEvent = { target: { value: 'train' } };
+    inputBox.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+});
